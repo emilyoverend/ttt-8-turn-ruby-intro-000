6bb415964e830e98c6c09d4f5fe5b3b2aa471b1e
Emily Overend <eo17233@gmail.com>
2021-01-15 23:14:00 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,38 +7,32 @@ def display_board(board)
 end
 
 def input_to_index(user_input)
-  index = user_input.to_i
-  index -= 1
-  return index
+  new_user_input = user_user.to_i
+  new_user_input -= 1
+  return new_user_input
+end
+
+def move(array, index, value = "X")
+  array[index] = value
+  return array
 end
 
 def valid_move?(board, index)
-  def position_taken?(board, ind)
-    if board[ind] == " " || board[ind] == "" || board[ind] == nil
-      return false
-    else
-      return true
-    end
-  end
-
-def on_board? (num)
-  if num.between?(0, 8) == true
+  if !position_taken?(board, index) && (index).between?(0,8)
     return true
   else
     return false
   end
 end
 
-if (position_taken?(board,index)) == false && (on_board?(index) == true)
-  return true
-else
-  return false
-end
-end
-
-def move(board, index, character = "X")
-  board[index] = character
-  return board
+def position_taken?(board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    return false
+  elsif board[index] == "X" || board[index] == "O"
+    return true
+  else 
+    return false
+  end
 end
 
 def turn(board)
